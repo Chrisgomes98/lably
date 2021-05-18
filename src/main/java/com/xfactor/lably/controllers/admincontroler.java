69a2ece6255974f47d8e22e6baa633d6d6740720
Chrisgomes98 <cgomes2k18@gmail.com>
2021-05-18 15:55:32 +0530

    commit 4

--- src/main/java/com/xfactor/lably/controllers/admincontroler.java
+++ src/main/java/com/xfactor/lably/controllers/admincontroler.java
@@ -1,9 +1,11 @@
 package com.xfactor.lably.controllers;
 
-import java.util.ArrayList;
+import java.util.*;
 
+import com.xfactor.lably.Repository.AdminRepository;
 import com.xfactor.lably.entity.admin;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,36 +17,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/admin")
 public class admincontroler {
 
-    ArrayList<admin> admins = new ArrayList<>();
+    @Autowired
+    AdminRepository adminRepository;
 
     @PostMapping("/addAdmin")
     public admin addAdmin(@RequestBody admin A1) {
-        admins.add(A1);
-        return A1;
+        admin PersistedAdmin= adminRepository.save(A1);
+        return PersistedAdmin;
     }
 
-    @GetMapping("/getAdmin")
-    public ArrayList<admin> getAdmin() {
-        for(int i =0;i<5;i++)
-        {
-            admin obj=new admin();
-            obj.setName("kkk");
-            obj.setPassword("lolz");
-            obj.setUser("k.k.k");
-            admins.add(obj);
-        }
-        return admins;
+    @GetMapping("/getAdmins")
+    public List<admin> getAdmins() {
+        List<admin> persistAdmins=adminRepository.findAll();
+        return persistAdmins;
     }
 
     @GetMapping("/getAdmin/{name}")
     public admin getAdmin(@PathVariable String name) {
-        admin res=new admin();
-        for (admin k : admins) 
-        { 
-            if(k.getName().equals(name))
-            {
-                return k;
-            }
+        admin res=null;
+        List<admin> persistAdmins=adminRepository.findAll();
+        for (admin x : persistAdmins)
+        {
+            if(x.getName().equalsIgnoreCase(name))
+                return x;
         }
         return res;
     }
