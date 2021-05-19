23c74e789a4c2bcfccb35f33925e4ef09cf9e46b
Chrisgomes98 <cgomes2k18@gmail.com>
2021-05-19 17:25:56 +0530

    lab issue

--- src/main/java/com/xfactor/lably/entity/costumer.java
+++ src/main/java/com/xfactor/lably/entity/costumer.java
@@ -1,11 +1,28 @@
 package com.xfactor.lably.entity;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Tbl_costumer")
 public class costumer {
     private String name;
     private String address;
     private String age;
     private String gender;
+
+
+    @Id
+    @GeneratedValue
+    private long Id;
     
+    
+    public long getId() {
+        return Id;
+    }
+    public void setId(long id) {
+        Id = id;
+    }
     public String getName() {
         return name;
     }
