61c586432f0c14870cd7f38db87c023fa048d556
Chrisgomes98 <cgomes2k18@gmail.com>
2021-05-19 17:39:53 +0530

    lets see

--- src/main/java/com/xfactor/lably/entity/Lab.java
+++ src/main/java/com/xfactor/lably/entity/Lab.java
@@ -1,10 +1,46 @@
 package com.xfactor.lably.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="Tbl_Lab")
+public class Lab
+{
+
+    @Id
+    @GeneratedValue
+    private long Id;
     private String name;
     private String address;
     private String pincode;
+    private String phone;
+    
 
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long id) {
+        Id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String address()
+    {
+        return address;
+    }
+
+    public void setAddress(String address)
+    {
+        this.address=address;
+    }
     public String getName() {
         return name;
     }
