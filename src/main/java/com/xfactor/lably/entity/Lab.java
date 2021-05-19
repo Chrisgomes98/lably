ff4205106090274975decfea400a535135f6d1af
Chrisgomes98 <cgomes2k18@gmail.com>
2021-05-19 13:45:44 +0530

    minor

--- src/main/java/com/xfactor/lably/entity/Lab.java
+++ src/main/java/com/xfactor/lably/entity/Lab.java
@@ -1,16 +1,13 @@
 package com.xfactor.lably.entity;
 
-public class Lab {
 
     private String name;
-    private String phone;
     private String address;
     private String pincode;
 
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
@@ -18,23 +15,12 @@ public class Lab {
     public String getPhone() {
         return phone;
     }
-
     public void setPhone(String phone) {
         this.phone = phone;
     }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
     public String getPincode() {
         return pincode;
     }
-
     public void setPincode(String pincode) {
         this.pincode = pincode;
     }
