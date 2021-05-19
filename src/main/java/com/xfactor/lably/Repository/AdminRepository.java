2edc4c86bb5f8de1018c8ead706c2375478c9468
Chrisgomes98 <cgomes2k18@gmail.com>
2021-05-19 13:06:03 +0530

    dbms connect

---
+++ src/main/java/com/xfactor/lably/Repository/AdminRepository.java
@@ -0,0 +1,7 @@
+package com.xfactor.lably.Repository;
+import com.xfactor.lably.entity.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRepository extends JpaRepository<admin, Long >{
+    
+}
--- src/main/java/com/xfactor/lably/entity/admin.java
+++ src/main/java/com/xfactor/lably/entity/admin.java
@@ -1,12 +1,30 @@
 package com.xfactor.lably.entity;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Tbl_Admin")
 public class admin {
+
     private String name;
     private String user;
+
+    @Column(name = "PSWD")
     private String password;
+
+    @Id
+    @GeneratedValue
+    private long Id;
     
     public String getName() {
         return name;
+    }
+    public long getId() {
+        return Id;
+    }
+    public void setId(long id) {
+        Id = id;
     }
     public void setName(String name) {
         this.name = name;
